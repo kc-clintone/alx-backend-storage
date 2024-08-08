b0d6d089080f02f8c0768a16e2547777fa0e7b41
kc-clintone <kayseeclintone@gmail.com>
2024-08-08 13:45:14 +0000

    Task 2: Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,12 +2,7 @@
 -- Step 1: Import the metal_bands table (assume it is already imported and available in the database).
 -- Step 2: Query to rank the country origins based on the total number of fans.
 
-SELECT
-    origin,
-    SUM(nb_fans) AS total_fans
-FROM
-    metal_bands
-GROUP BY
-    origin
-ORDER BY
-    total_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
