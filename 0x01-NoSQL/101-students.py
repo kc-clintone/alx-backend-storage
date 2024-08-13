c7b396291f75785bbbe05cb696cb934143653b81
kc-clintone <kayseeclintone@gmail.com>
2024-08-13 12:28:55 +0000

    corrections

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -3,32 +3,22 @@
 Top students
 """
 
-from typing import List, Dict
-from pymongo.collection import Collection
-
-
-def top_students(mongo_collection: Collection) -> List[Dict]:
+def top_students(mongo_collection):
     """
-    Returns a list of all students in a collection, sorted by their average score.
-    Args:
-        mongo_collection: The pymongo collection object.
-
-    Returns:
-        A list of dictionaries, where each dictionary represents a student document
-        with an additional field "averageScore", sorted in descending order by the average score.
+    Returns a list of all students in a collection, sorted by their average scores
     """
-    students = mongo_collection.aggregate(
-        [
-            {
-                '$project': {
-                    'name': 1,
-                    'averageScore': {'$avg': '$topics.score'},
-                    'topics': 1,
+    students = mongo_collection.aggregate([
+        {
+            '$project': {
+                'name': 1,
+                'topics': 1,
+                'averageScore': {
+                    '$avg': '$topics.score',
                 },
             },
-            {
-                '$sort': {'averageScore': -1},
-            },
-        ]
-    )
+        },
+        {
+            '$sort': {'averageScore': -1},
+        },
+    ])
     return list(students)
