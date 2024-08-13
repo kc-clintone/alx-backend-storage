121f7b480985bf55784f1ea7cd0494e0bf4387a3
kc-clintone <kayseeclintone@gmail.com>
2024-08-13 11:45:13 +0000

    Task 9: Insert a document in python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Insert a document in Python
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection.
+    """
+    res = mongo_collection.insert_one(kwargs)
+    return res.inserted_id
