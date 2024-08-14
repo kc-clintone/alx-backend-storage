19668ab9b79b0992d16529e1134e2485553caf3a
kc-clintone <kayseeclintone@gmail.com>
2024-08-14 22:03:15 +0000

    Task 0: Part one - Reading from Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#/usr/bin/env python3
+"""
+Class module
+"""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    The class for this module
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
