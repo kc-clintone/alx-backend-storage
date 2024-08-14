0d37b804f651d966768a1b17c17f51102fbe2759
kc-clintone <kayseeclintone@gmail.com>
2024-08-14 22:17:37 +0000

    Task 0: Part one - Reading from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Cache class module: Writing strings to Redis
 """
 
-from typing import Union
+from typing import Union, Callable, Optional
 import redis
 import uuid
 
@@ -24,3 +24,17 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Union[str, int, float]]] = None) -> Optional[Union[str, int, float, bytes]]:
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, fn=int)
