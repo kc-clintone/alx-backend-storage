182ee5d5f2c9c3abd29279d5aa2d355ceaffa63a
kc-clintone <kayseeclintone@gmail.com>
2024-08-14 22:06:57 +0000

    Task 0: Part one - Writting strings to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,10 +12,14 @@ class Cache:
     """
     The class for this module
     """
+
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    """
+    Stores a value in redis and returns a key
+    """
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
